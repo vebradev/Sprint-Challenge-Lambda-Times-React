2923d73de59d6f3c8fbe8025d7230216982d6f14
vebradev <justinas@vebra.dev>
2019-06-07 11:15:42 +0300

    Pass props from Tabs component to Tab.

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -10,7 +10,12 @@ const Tabs = props => {
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
         {props.tabs.map(tab => (
-          <Tab tab={tab} key={uuid()} />
+          <Tab
+            tab={tab}
+            key={uuid()}
+            selectTabHandler={props.selectTabHandler}
+            selectedTab={props.selectedTab}
+          />
         ))}
       </div>
     </div>
